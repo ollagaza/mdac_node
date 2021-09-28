be142775b9b8cf2d4a79339feb92da0830ac3ddb
sgbae <pear.gyu@gmail.com>
2021-09-28 12:32:57 +0900

    Update apifile.js

--- src/routes/d1/apifile.js
+++ src/routes/d1/apifile.js
@@ -22,13 +22,28 @@ routes.get('/', Wrap(async (req, res) => {
 routes.post('/uploadorgfile', Auth.isAuthenticated(Role.LOGIN_USER), Wrap(async (req, res) => {
   req.accepts('application/json')
   try{
+    if (!req.files) {
+      console.log('not files');
+
+      const output = new StdObject(-1, 'not exist files', 200);
+      // output.add('data', '');
+      res.json(output);
+    } else {
+      console.log('files');
+      let f = req.files.uploadFile;
+      // res.send({
+
+      // });
+      const output = new StdObject(0, 'success', 200);
+      output.add('data', '');
+      res.json(output);
+    }
+
       // const project_seq = req.query.pseq;
       // console.log(project_seq);
 
       // const result = await ProjectService.GetDivisions(DBMySQL, project_seq);
-      const output = new StdObject(0, 'success', 200);
-      output.add('data', '');
-      res.json(output);
+      
   } catch (e) {
       logger.error('/apifile/uploadorgfile', e)
       if (e.error < 0) {
