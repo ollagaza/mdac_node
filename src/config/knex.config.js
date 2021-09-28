97675b054be6ca9988caa22bd4dd262799b82b6d
ollagaza <ollagaza@hanmail.net>
2021-09-28 11:58:14 +0900

    merge 동기화

--- src/config/knex.config.js
+++ src/config/knex.config.js
@@ -3,16 +3,10 @@ export default {
     client: 'mysql',
     connection: {
       host: '127.0.0.1',
-<<<<<<< HEAD
-      database: 'jiinmteg',
-      user: 'root',
-      password: '_media_',
-=======
       port: 3307,
       database: 'datamanager',
       user: 'root',
       password: '1234',
->>>>>>> 2f6467e9af1401a91d29a4baf4010cc67056f9c6
       timezone: 'UTC'
     },
     pool: {
@@ -27,16 +21,10 @@ export default {
     client: 'mysql',
     connection: {
       host: '127.0.0.1',
-<<<<<<< HEAD
-      database: 'jiinmteg',
-      user: 'root',
-      password: '_media_',
-=======
       port: 3307,
       database: 'datamanager',
       user: 'root',
       password: '1234',
->>>>>>> 2f6467e9af1401a91d29a4baf4010cc67056f9c6
       timezone: 'UTC'
     },
     pool: {
@@ -52,16 +40,10 @@ export default {
     client: 'mysql',
     connection: {
       host: '127.0.0.1',
-<<<<<<< HEAD
-      database: 'ropmteg',
-      user: 'root',
-      password: '_media_',
-=======
       port: 3307,
       database: 'datamanager',
       user: 'root',
       password: '1234',
->>>>>>> 2f6467e9af1401a91d29a4baf4010cc67056f9c6
       timezone: 'UTC'
     },
     pool: {
