1ae85e23965ee40ccc2cb1dd95a9fd1d8f4a810e
ollagaza <ollagaza@hanmail.net>
2021-11-10 12:06:28 +0900

    회원 작업 count 수정

--- src/database/mysql/member/Member_Model.js
+++ src/database/mysql/member/Member_Model.js
@@ -18,7 +18,8 @@ export default class MemberModel extends MySQLModel {
   }
 
   getWokerList = async (is_used) => {
-    const select = ['seq', 'user_id', 'user_name', 'reg_date', 'email', knex.raw('\'100\' as wcount') , knex.raw('\'10\' as bcount') , knex.raw('\'30\' as ccount') , knex.raw('\'430\' as dcount')]
+    // const select = ['seq', 'user_id', 'user_name', 'reg_date', 'email', knex.raw('\'100\' as wcount') , knex.raw('\'10\' as bcount') , knex.raw('\'30\' as ccount') , knex.raw('\'430\' as dcount')]
+    const select = ['seq', 'user_id', 'user_name', 'reg_date', 'email', knex.raw('IFNULL((SELECT SUM(label_cnt) FROM job WHERE member.seq = job.labeler_member_seq AND job.labeler_jobdate IS NULL),0) AS labelcnt') , knex.raw('IFNULL((SELECT COUNT(*) FROM job_worker WHERE member.seq = job_worker.job_member_seq AND job_date IS NULL AND (job_name = \'B\' OR job_name = \'C\' OR job_name = \'D\')),0) AS checkcnt')]
     const oKnex = this.database.select(select).from(this.table_name).where('is_used',is_used);
     const result = {};
     try{
