c20c02a567c11f0e39c2de6daf8602916992edde
ollagaza <ollagaza@hanmail.net>
2021-11-10 12:06:11 +0900

    할당로그쿼리수정

--- src/database/mysql/datamanager/JobWoker_Model.js
+++ src/database/mysql/datamanager/JobWoker_Model.js
@@ -125,27 +125,56 @@ export default class MemberModel extends MySQLModel {
         result.error = 0;
       }
       if (job_seq) {
-        const select = ['jw.*', 'jo.status AS jostatus', 'm.user_name', 'mreg.user_name as reg_member',
+        // const select1 = ['job.seq as seq', 'job.project_seq','job.seq as job_seq','1 as result_file_pair_key','job.class_seq', 
+        //   `'A' as job_name`,`'A0' as job_status`,'job.labeler_member_seq as job_member_seq','job.labeler_regdate as reg_date',
+        //   'job.labeler_jobdate as job_date','NULL as reject_date','job.reg_member_seq','job.reject_act','job.reject_seq',
+        //   `'A0' as jobstatus`,'m.user_name', 'mreg.user_name as reg_member', 'mj.user_name as labeler_member',
+        //   'labeler_regdate as labeler_regdate']
+        const select1 = knex.raw(`job.seq as seq, job.project_seq, job.seq as job_seq, 1 as result_file_pair_key, job.class_seq, 
+        'A' as job_name, 'A' as job_status, job.labeler_member_seq as job_member_seq, job.labeler_regdate as reg_date,
+        NULL  as job_date, NULL as reject_date, job.reg_member_seq, job.reject_act, job.reject_seq,
+        'A0' as jobstatus, m.user_name, mreg.user_name as reg_member, mj.user_name as labeler_member,
+        labeler_regdate as labeler_regdate`)
+
+        const select2 = ['jw.*', 'jo.status AS jostatus', 'm.user_name', 'mreg.user_name as reg_member',
           'mj.user_name as labeler_member', 'jo.labeler_regdate as labeler_regdate']
         const oKnex = this.database
-          .select(select)
-          .from('job as jo')
-          .leftJoin('job_worker as jw', function () {
-            this.on('jo.seq', '=', 'jw.job_seq')
+          .unionAll(function () {
+            this.select([select1])
+              .from('job')
+              .leftJoin('member as m', function () {
+                this.on('m.seq', '=', 'job.labeler_member_seq')
+              })
+              .leftJoin('member as mreg', function () {
+                this.on('mreg.seq', '=', 'job.reg_member_seq')
+              })
+              .leftJoin('member as mj', function () {
+                this.on('mj.seq', '=', 'job.labeler_member_seq')
+              })
+              .where('job.seq', job_seq)
+          }, true)
+          
+          .unionAll(function () {
+            this.select(select2)
+              .from('job as jo')
+              .join('job_worker as jw', function () {
+                this.on('jo.seq', '=', 'jw.job_seq')
+              })
+              .leftJoin('member as mj', function () {
+                this.on('jo.labeler_member_seq', '=', 'mj.seq')
+              })
+              .leftJoin('member as m', function () {
+                this.on('jw.job_member_seq', '=', 'm.seq')
+              })
+              .leftJoin('member as mreg', function () {
+                this.on('jw.reg_member_seq', '=', 'mreg.seq')
+              })
+              .where('jo.seq', job_seq)
+            if (req_body.file_type === 'v') {
+              this.andWhere('result_file_pair_key', req_body.ref_pair_key);
+            }
           })
-          .leftJoin('member as mj', function () {
-            this.on('jo.labeler_member_seq', '=', 'mj.seq')
-          })
-          .leftJoin('member as m', function () {
-            this.on('jw.job_member_seq', '=', 'm.seq')
-          })
-          .leftJoin('member as mreg', function () {
-            this.on('jw.reg_member_seq', '=', 'mreg.seq')
-          })
-          .where('jo.seq', job_seq);
-        if (req_body.file_type === 'v') {
-          oKnex.andWhere('result_file_pair_key', req_body.ref_pair_key);
-        }
+
         oKnex.orderBy('job_status');
         result.list = await oKnex;
         result.error = 0;
